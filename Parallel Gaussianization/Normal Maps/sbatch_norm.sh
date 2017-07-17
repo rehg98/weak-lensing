51d364930314b8a8b0f264eb581675197f1b09dc
Ryan Golant <rgolant@princeton.edu>
2017-07-17 14:21:27 -0400

    Added files for Gaussianized power spectra of normal maps; renamed some files

---
+++ Parallel Gaussianization/Normal Maps/sbatch_norm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -N 1 
+#SBATCH -n 28
+#SBATCH --ntasks-per-node=28
+#SBATCH -t 1:00:00
+#SBATCH --output=SNRout.out
+#SBATCH --error=SNRerr.err
+#SBATCH --mail-type=all
+#SBATCH --mail-user=rgolant@princeton.edu
+module load openmpi
+module load intel
+module load anaconda3/4.3.0
+srun -n 28 python SNR_norm.py
+wait
